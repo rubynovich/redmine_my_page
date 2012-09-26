9f7df8a9b505606272a3c627c7b3e8670f666ebf
Rubynovich <roman@shipiev.me>
2012-09-26 12:59:23 +0400

    first

---
+++ init.rb
@@ -0,0 +1,10 @@
+require 'redmine'
+
+Redmine::Plugin.register :redmine_my_page do
+  name 'Redmine My Page plugin'
+  author 'Roman Shipiev'
+  description 'Redmine plugin, which makes "my page" more informative'
+  version '0.0.1'
+  url 'http://github.com/rubynovich/redmine_my_page'
+  author_url 'http://roman.shipiev.me'
+end
